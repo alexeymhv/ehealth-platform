abd42a922a4b9ac892045a3e7b4136ee307f2189
Alex Alex <alex#alex.com>
2016-05-14 22:46:07 +0300

    Removed data.js module. And added 'service mysql stop' to mysqldb launch script

--- bin/run_mysqldb.sh
+++ bin/run_mysqldb.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+service mysql stop
 
 docker rm -f mysqldb
 docker run --name mysqldb -v ~/application_data/ehealth/mysqldb:/var/lib/mysql --net=host -p 127.0.0.2:3306:3306 -e MYSQL_ROOT_PASSWORD=rootpass -d mysql
